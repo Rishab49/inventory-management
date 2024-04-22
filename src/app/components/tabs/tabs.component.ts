48797b283c83a4c0cb942073c8a2dd0eecf9ac72
Rishab49 <rishab.rishu49@gmail.com>
2024-04-22 19:29:50 +0530

    first

---
+++ src/app/components/tabs/tabs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {  MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-tabs',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './tabs.component.html',
+  styleUrl: './tabs.component.css'
+})
+export class TabsComponent {
+
+}
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { ToggleComponent } from '../components/toggle/toggle.component';
+import { TabsComponent } from '../components/tabs/tabs.component';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   // add components here to use in the html template
-  imports: [HeaderComponent,MatIconModule,ToggleComponent],
+  imports: [HeaderComponent,MatIconModule,ToggleComponent,TabsComponent],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
